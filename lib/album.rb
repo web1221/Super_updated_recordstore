188c691de17a15a4940c307ae1dd01dc7952e986
Student <example@example.com>
2019-12-16 10:21:01 -0800

    add crud functionality

--- lib/album.rb
+++ lib/album.rb
@@ -1,19 +1,42 @@
 class Album
 
-@@albums = {}
+  @@albums = {}
+  @@total_rows = 0
 
-attr_accessor :name
+  attr_reader :id, :name
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
 
-def self.all()
-  @@albums.values
-end
+  def self.all()
+    @@albums.values
+  end
 
-def ==(other_album)
-  self.name.eql?(other_album.name)
-end
+  def save()
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(other_album)
+    self.name.eql?(other_album.name)
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+
+  def update(name)
+    @name = name
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
 
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -6,13 +6,54 @@ require('rspec')
 describe('.#Album') do
 
   before(:each) do
-    @album1 = Album.new("Blue")
-    @album2 = Album.new("Red")
+    @album1 = Album.new("Blue", nil)
+    @album2 = Album.new("Red", nil)
+    @album1.save
+    @album2.save
   end
 
-  describe('.all') do
-    it(('returns an empty array when there are no albums')) do
+  after(:each) do
+    Album.clear
+  end
+
+  describe('#==') do
+    it("is the same album if it has the same attributes") do
+      album3 = Album.new("Blue", nil)
+      expect(@album1).to(eq(album3))
+    end
+  end
+
+
+  describe('#save') do
+    it ("saves an album") do
+      expect(Album.all).to(eq([@album1, @album2]))
+    end
+  end
+
+  describe(".clear") do
+    it("clears all albums") do
+      Album.clear
       expect(Album.all).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it("finds an album by id") do
+      expect(Album.find(@album1.id)).to(eq(@album1))
+    end
+  end
+
+  describe('#update') do
+    it("updates an album by id") do
+      @album1.update("Yellow")
+      expect(@album1.name).to(eq('Yellow'))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an album by id") do
+      @album1.delete()
+      expect(Album.all).to(eq([@album2]))
     end
   end
 
